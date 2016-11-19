63d15679a9fe0897cc4ba6e8adc52df0bf20f13f
Prateek Bagrecha <PBagrecha.extern@chip.de>
2016-11-19 23:32:48 +0100

    DB changed

--- config/config.js
+++ config/config.js
@@ -3,8 +3,13 @@ Config.db = {};
 Config.app={};
 Config.auth = {};
 
-Config.db.host = 'localhost:27017';
-Config.db.name = 'MatchResults';
+//Config.db.host = 'localhost:27017';ds013545.mlab.com:13545/skydb
+Config.db.host = 'ds013545.mlab.com:13545';
+//Config.db.name = 'MatchResults';
+Config.db.name = 'skydb';
+
+Config.db.user = 'sebateam9';
+Config.db.pass = '123';
 
 // Use environment defined port or 3000
 Config.app.port = process.env.PORT || 3000;
