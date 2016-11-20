b900cc40514b5a3cd51cb84cbffe4eac55343864
Akash Manjunath <akash.manjunath@gmail.com>
2016-11-20 12:48:54 +0100

    updated return

--- app.js
+++ app.js
@@ -46,40 +46,54 @@ app.post('/query', function(req,res) {
 
     var parameters = request.result.parameters;
 
-    var quantifier = parameters.quantifier.toLowerCase();
-
-    var playerNames = ['Phillip Lahm','Thomas Müller','Robert Lewandowski', 'David Alaba', 'Thiago Alcántara',
+    var playerNames = ['Phillip Lahm', 'Thomas Müller', 'Robert Lewandowski', 'David Alaba', 'Thiago Alcántara',
         'Arjen Robben', 'Douglas Costa', 'Arturo Vidal', 'Xabi Alonso'];
 
-    var result = "";
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 
-    if(quantifier.indexOf('which')>-1){
-        var num = getRandomInt(0,8);
-        var param = getRandomInt(0,2);
-        console.log(playerNames[num]+" has the highest number of "+parameters.player_actions+" so far with "+num);
-        result = playerNames[num]+" has the highest number of "+parameters.player_actions+" so far with "+num;
-    }else if(quantifier.indexOf('what happened')>-1){
+    if(parameters.quantifier==='undefined') {
+
+        var quantifier = parameters.quantifier.toLowerCase();
+
+        var result = "";
+
+        if (quantifier.indexOf('which') > -1) {
+            var num = getRandomInt(0, 8);
+            var param = getRandomInt(0, 2);
+            console.log(playerNames[num] + " has the highest number of " + parameters.player_actions + " so far with " + num);
+            result = playerNames[num] + " has the highest number of " + parameters.player_actions + " so far with " + num;
+        } else {
+            var num;
+            if ((parameters.quantifier.indexOf('goal') > -1) || (parameters.quantifier.indexOf('assist')) > -1) {
+                num = getRandomInt(0, 3);
+            } else {
+                num = getRandomInt(0, 7);
+            }
+            console.log(playerNames[num] + " has the highest number of " + parameters.player_actions + " so far with " + num);
+            result = parameters.player_name + " has " + num + " number of " + parameters.player_actions + " so far.";
+        }
+
+
+    } else {
         var num;
 
         var curTime = new Date();
         var tmpTime = curTime;
 
         var tmpTime1 = new Date(curTime);
-        tmpTime1.setMinutes(curTime.getMinutes()-1);
+        tmpTime1.setMinutes(curTime.getMinutes() - 1);
 
         var tmpTime2 = new Date(curTime);
-        tmpTime2.setMinutes(curTime.getMinutes()-2);
+        tmpTime2.setMinutes(curTime.getMinutes() - 2);
 
 
         var tim1 = dateFormat(tmpTime, "yyyy-mm-dd h:MM:ss");
         var tim2 = dateFormat(tmpTime1, "yyyy-mm-dd h:MM:ss");
         var tim3 = dateFormat(tmpTime2, "yyyy-mm-dd h:MM:ss");
-
-        if((parameters.quantifier.indexOf('goal')>-1)||(parameters.quantifier.indexOf('assist'))>-1){
-            num = getRandomInt(0,3);
-        } else {
-            num = getRandomInt(0,7);
-        }
 
         var tmp1 = tim1 + " " + playerNames[8] + " put in a good cross. Good chance!!!";
         var tmp2 = tim2 + " " + playerNames[7] + " lost possession.";
@@ -88,23 +102,7 @@ app.post('/query', function(req,res) {
         console.log(result);
         result = tmp1 + "\n" + tmp2 + "\n" + tmp3 + "\n";
 
-    }else{
-        var num;
-        if((parameters.quantifier.indexOf('goal')>-1)||(parameters.quantifier.indexOf('assist'))>-1){
-            num = getRandomInt(0,3);
-        } else {
-            num = getRandomInt(0,7);
-        }
-        console.log(playerNames[num]+" has the highest number of "+parameters.player_actions+" so far with "+num);
-        result = parameters.player_name + " has " + num + " number of " +parameters.player_actions +" so far.";
     }
-
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
-
     res.statusCode= 200;
 
 
